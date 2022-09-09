ca09eb0b734fb493e1cd3c0cbd7e689ff9e7049c
tien thuan <tienthuan05082002@gmail.com>
2022-09-10 05:49:43 +0700

    add multi delete Recycle View

--- app/src/main/java/thuan/todolist/feature_todo/adapter/ToDoAdapter.kt
+++ app/src/main/java/thuan/todolist/feature_todo/adapter/ToDoAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.selection.SelectionTracker
@@ -27,9 +28,42 @@ class ToDoAdapter(private val viewModel: ToDoViewModel) :
             todo: ToDo,
             viewModel: ToDoViewModel
         ) {  // fun bind is used to bind data to the view
+            Log.i("adapter", adapterPosition.toString())
+            bindData(todo, viewModel)
+            startAnimation()
+            checkItemSelection(todo)
+        }
+
+        private fun bindData(
+            todo: ToDo,
+            viewModel: ToDoViewModel
+        ) {
             binding.todo = todo
             binding.viewModel = viewModel
             binding.executePendingBindings()
+        }
+
+        private fun startAnimation() {
+            // set animation fade in for each item
+//        holder.binding.root.alpha = 0f
+//        holder.binding.root.translationY = 100f
+//        holder.binding.root.animate().alpha(1f).translationY(0f).setDuration(500).start() gây lỗi khi bind lại
+            val animation: Animation =
+                AnimationUtils.loadAnimation(binding.root.context, R.anim.anim_fade_in)
+            binding.root.startAnimation(animation)
+        }
+
+        fun detachAnimation() {
+            binding.root.clearAnimation()
+        }
+
+        private fun checkItemSelection(todo: ToDo) {
+            if (tracker?.isSelected(todo.id.toLong()) == true) {
+                Log.i("adapter", "checkItemSelection: ")
+                binding.root.alpha = 0.5f
+            } else {
+                binding.root.alpha = 1f
+            }
         }
 
         fun getItemDetails(): ItemDetailsLookup.ItemDetails<Long> {
@@ -58,19 +92,11 @@ class ToDoAdapter(private val viewModel: ToDoViewModel) :
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
         holder.bind(dataList[position], viewModel)
         Log.i("adapter", "onBindViewHolder" + dataList[position].id)
-
-        // set animation fade in for each item
-//        holder.binding.root.alpha = 0f
-//        holder.binding.root.translationY = 100f
-//        holder.binding.root.animate().alpha(1f).translationY(0f).setDuration(500).start() gây lỗi khi bind lại
-        val animation: Animation =
-            AnimationUtils.loadAnimation(holder.itemView.context, R.anim.anim_fade_in)
-        holder.binding.root.startAnimation(animation)
     }
 
     override fun onViewDetachedFromWindow(holder: ToDoViewHolder) {
         super.onViewDetachedFromWindow(holder)
-        holder.binding.root.clearAnimation()
+        holder.detachAnimation()
 
     }
 
