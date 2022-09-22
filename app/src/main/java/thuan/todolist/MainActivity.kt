5e071ecee3053f8d55513868802566f425f9e7b0
tien thuan <tienthuan05082002@gmail.com>
2022-09-23 00:36:45 +0700

    Fix warning

--- app/src/main/java/thuan/todolist/MainActivity.kt
+++ app/src/main/java/thuan/todolist/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import thuan.todolist.databinding.ActivityMainBinding
-import thuan.todolist.di.Injection
-import thuan.todolist.feature_todo.ui.home.ToDoViewModel
-import thuan.todolist.feature_todo.ui.home.ToDoViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,40 +29,9 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
     }
 
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("MainActivity", "onDestroy")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("MainActivity", "onStop")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("MainActivity", "onPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("MainActivity", "onResume")
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.i("MainActivity", "onStart")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("MainActivity", "onRestart")
     }
 }
