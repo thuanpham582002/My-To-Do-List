56341f34034c8d5dc40c8c0e9db6426343e6453b
tien thuan <tienthuan05082002@gmail.com>
2022-09-29 21:49:09 +0700

    Update action notification with intent, deeplink

---
+++ app/src/main/java/thuan/todolist/feature_todo/domain/service/constant/Constant.kt
@@ -0,0 +1,8 @@
+package thuan.todolist.feature_todo.domain.service.constant
+
+const val TODO_TITLE = "todo_title"
+const val TODO_CHANNEL_ID = "channel_id"
+const val TODO_DESCRIPTION = "todo_message"
+const val TODO_ID = "todo_id"
+const val ACTION_DONE = "done_action"
+const val ACTION_DELETE = "delete_action"
